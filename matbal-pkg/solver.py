0d9c626fab63e2d8ed9610f4e138bcb19f2902f6
A_korobeynikov <korobeyAV@mail.ru>
2023-04-14 18:43:40 +0500

    v 1 1 1

--- matbal-pkg/solver.py
+++ matbal-pkg/solver.py
@@ -1,6 +1,7 @@
 from mat_balance import MaterialBalance
 from proxy_instance import ProxyInstance
 from scipy.optimize import minimize
+from geneticalgorithm import geneticalgorithm as ga
 import numpy as np
 import pandas as pd
 from typing import Tuple, Any
@@ -56,6 +57,48 @@ class SolverMatbal:
             p_initial=p_initial)
         return res_mat_bal, sol
 
+    def optimize_ga(self,border_j1d_like_ones:np.ndarray,lower=None, upper=None, start=None, stop=None)->Tuple[ProxyInstance,Any]:
+        lower_value = 0 if lower is None else lower
+        upper_value = 10 if upper is None else upper
+
+        first_step = 0 if start is None else start
+        last_step = -1 if start is None else stop
+
+        p_initial = self.rpr[first_step]
+        rgpip_initial = self.rgip[first_step]
+        press_model = self.rpr[first_step:last_step,:]
+        wgp =self.wgp[first_step:last_step,:]
+        def fun(j_1d):
+            j_2d = ProxyInstance.get_j2d(j_1d, self.n)
+            res = ProxyInstance.get_press_all_dates(
+                p_initial=p_initial,
+                wgp_by_dates= wgp,
+                J_ij=j_2d,
+                rgip_initial=rgpip_initial)
+            press_calc = np.array([r.x for r in res])            
+            return np.abs(press_calc - press_model).mean()
+        bnds = np.array([
+                [0,0] if marker == 0 else [lower_value,upper_value]
+            for marker in border_j1d_like_ones])
+        x0 = np.random.rand(border_j1d_like_ones.shape[0])
+        x0[np.where(border_j1d_like_ones==0)] = 0        
+        algorithm_param = {'max_num_iteration': 2000,\
+                   'population_size':500,\
+                   'mutation_probability':.01,
+                   'elit_ratio': 0.001, #Было ,1
+                   'crossover_probability': 0.5,\
+                   'parents_portion': 0.002, #было ,3
+                   'crossover_type':'uniform',\
+                   'max_iteration_without_improv':200}
+        model=ga(function=fun,dimension=x0.shape[0],variable_type='real',variable_boundaries=bnds,
+                algorithm_parameters=algorithm_param)
+        model.run()
+        return model
+
+
+
+
+
 if __name__=="__main__":
     rpr = np.array(
     [[600.,         600.,         600.,         600.        ],
@@ -95,14 +138,12 @@ if __name__=="__main__":
         [   8.50282927,    8.64896416,    9.34308766,   13.50511891,],
         [   8.96310072,    9.17648347,    9.53450759,   12.32590822]])
     solver = SolverMatbal(wgp_all, rpr, rgip) 
-    for _ in range(10):
-        m_bal, sol = solver.optimize(np.ones((6)))
-        print(sol.fun, sol.x)
-    press_calc = m_bal.calc_press(wgp_all)
+    # for _ in range(10):к
+    #     m_bal, sol = solver.optimize(np.ones((6)))
+    #     print(sol.fun, sol.x)
+    # press_calc = m_bal.calc_press(wgp_all)
     # print((press_calc - rpr).reshape(-1).shape)
     # print(pd.DataFrame({'data':(np.abs(press_calc - rpr)).reshape(-1)}).describe())
+    print(solver.optimize_ga(np.ones((6))))
     
 
-
-    
- 
